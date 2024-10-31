dc4c819d29a1f1d02c091c53587c5a72e495ae46
AndersenIA <159605243+AndersenIA@users.noreply.github.com>
2024-10-31 13:26:12 +0100

    getting mobile nav to work

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ containers.forEach((container) => {
     }
   });
 });
+
+const mobileMenu = document.getElementsByClassName("mobile_nav-menu")[0];
+
+const openMenu = document.getElementsByClassName("icon-mobile-nav")[0];
+
+const closeMenu = document.getElementsByClassName("mobile-menu-close-icon")[0];
+
+openMenu.addEventListener("click", function () {
+  mobileMenu.classList.add("display-block");
+});
+
+closeMenu.addEventListener("click", function () {
+  mobileMenu.classList.remove("display-block");
+});
